9bf60f530e9933d8ba670b576ed5959d129d8c71
ForteScarlet <ForteScarlet@163.com>
2021-11-27 22:15:53 +0800

    request util

--- src/main/kotlin/love/forte/simbot/tencentguild/api/ApiRequestor.kt
+++ src/main/kotlin/love/forte/simbot/tencentguild/api/ApiRequestor.kt
@@ -14,11 +14,22 @@ import love.forte.simbot.tencentguild.TencentGuildApi
 import love.forte.simbot.tencentguild.err
 
 
+
+
 /**
  *
+ * 通过提供的参数，对此api进行请求并得到最终结果。
+ *
+ * @param client 提供一个 http client
+ * @param server 提供一个目标服务器路径
+ * @param token 提供一个 token.
+ * @param decoder 如果有，提供一个decoder用于通过 [TencentApi.resultDeserializer] 进行反序列化，而不是通过 [client] 进行。
+ *
+ * @throws love.forte.simbot.tencentguild.TencentApiException 如果响应码不在 200..300 范围内。
  */
 public suspend inline fun <reified R> TencentApi<R>.request(
     client: HttpClient,
+    server: Url,
     token: String,
     decoder: StringFormat? = null
 ): R {
@@ -40,8 +51,8 @@ public suspend inline fun <reified R> TencentApi<R>.request(
             api.route(routeBuilder)
             api.body?.also { b -> body = b }
 
-            protocol = TencentGuildApi.URL.protocol
-            host = TencentGuildApi.URL.host
+            protocol = server.protocol
+            host = server.host
             path(routeBuilder.apiPath)
             contentType(routeBuilder.contentType)
 
@@ -49,7 +60,7 @@ public suspend inline fun <reified R> TencentApi<R>.request(
 
     }
 
-    // check success
+    // check status
     val status = resp.status
     if (!status.isSuccess()) {
         val info = resp.receive<ErrInfo>()
