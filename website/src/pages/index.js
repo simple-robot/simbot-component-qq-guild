8e32537d0b5c96541f1cafc6ffaec93bd934dd33
ForteScarlet <ForteScarlet@163.com>
2023-02-11 14:48:16 +0800

    doc: website

---
+++ website/src/pages/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner, styles.heroBannerBg)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs">直奔文档📚</Link>
+        </div>
+          <div className={styles.buttons}>
+              <Link
+                  className="button button--outline button--md"
+                  to="https://github.com/simple-robot/simbot-component-tencent-guild">
+                  {<><span className="bi-github"> GitHub</span> </>}
+              </Link>
+          </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`欢迎!`}
+      description="提供QQ频道平台功能的simbot组件实现库 <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
