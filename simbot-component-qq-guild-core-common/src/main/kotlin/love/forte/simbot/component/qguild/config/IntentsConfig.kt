dd364ec4054d119bce6e46a917a2840c062034fa
ForteScarlet <ForteScarlet@163.com>
2023-04-03 17:34:49 +0800

    fix(core): 修复配置属性 `config.intents` 当 `type = nameBased` 时配置不生效的问题

--- simbot-component-qq-guild-core-common/src/main/kotlin/love/forte/simbot/component/qguild/config/IntentsConfig.kt
+++ simbot-component-qq-guild-core-common/src/main/kotlin/love/forte/simbot/component/qguild/config/IntentsConfig.kt
@@ -3,17 +3,22 @@
  *
  * This file is part of simbot-component-qq-guild.
  *
- * simbot-component-qq-guild is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * simbot-component-qq-guild is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
  *
- * simbot-component-qq-guild is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * simbot-component-qq-guild is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with simbot-component-qq-guild. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License along with simbot-component-qq-guild.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 
 package love.forte.simbot.component.qguild.config
 
-import io.ktor.util.*
-import kotlinx.serialization.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import love.forte.simbot.qguild.event.EventIntents
 import love.forte.simbot.qguild.event.Intents
 
@@ -66,16 +71,21 @@ public sealed class IntentsConfig {
 
         override val intents: Intents
             get() {
-                val intentsMap = EventIntents::class.sealedSubclasses
-                    .asSequence()
-                    .filter { it.simpleName != null && it.objectInstance != null }
-                    .associate {
-                        it.simpleName!!.toSnakeCase() to it.objectInstance!!.intentsValue
-                    }
-
                 if (names.isEmpty()) {
                     return Intents.ZERO
                 }
+
+                val intentsMap = mutableMapOf<String, Int>()
+                EventIntents::class.sealedSubclasses
+                    .asSequence()
+                    .filter { it.simpleName != null && it.objectInstance != null }
+                    .forEach {
+                        val intentsValue = it.objectInstance!!.intentsValue
+                        val simpleName = it.simpleName!!
+
+                        intentsMap[simpleName.toSnakeCase()] = intentsValue
+                        intentsMap[simpleName] = intentsValue
+                    }
 
                 return names
                     .map {
