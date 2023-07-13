2899a9142c72303e82eed4fbc14383aa35fd50c6
ForteScarlet <ForteScarlet@163.com>
2023-07-13 23:13:30 +0800

    feat(core): 支持使用环境变量配置 ticket

--- simbot-component-qq-guild-core-common/src/main/kotlin/love/forte/simbot/component/qguild/config/TicketConfiguration.kt
+++ simbot-component-qq-guild-core-common/src/main/kotlin/love/forte/simbot/component/qguild/config/TicketConfiguration.kt
@@ -66,14 +66,16 @@ public sealed class TicketConfiguration {
      * 或
      *
      * ```json
-     *      * {
-     *      *   "ticket": {
-     *      *     "appId": "appId-value",
-     *      *     "secret": "secret-value",
-     *      *     "token": "token-value"
-     *      *   }
-     *      * }
-     *      * ```
+     * {
+     *   "ticket": {
+     *     "appId": "appId-value",
+     *     "secret": "secret-value",
+     *     "token": "token-value"
+     *   }
+     * }
+     * ```
+     *
+     * > Note: 默认 `type` 的支持需要 simbot-core 3.2.0+
      *
      */
     @Serializable
@@ -103,7 +105,118 @@ public sealed class TicketConfiguration {
         }
     }
 
-    // TODO
+    /**
+     * 使用JVM参数或环境变量来读取 [QGBotFileConfiguration.Ticket] 中对应的配置类型。
+     *
+     * ```json
+     * {
+     *   "ticket": {
+     *     "type": "env",
+     *     "appId": "APP_ID",
+     *     "secret": "SECRET",
+     *     "token": "TOKEN",
+     *     "plain": false
+     *   }
+     * }
+     * ```
+     *
+     * [Env] 会首先尝试获取 JVM 参数，即运行时的 `-Dxxx=xxx` （也就是 [System.getProperty]），
+     * 当不存在时会尝试通过环境变量获取（即 [System.getenv]）。
+     *
+     * ## 原始输入
+     *
+     * 当 [plain] 为 `true` 时（默认为 `false`），如果某属性通过上述流程无法获取到值，则会尝试直接使用原始输入值。
+     *
+     * 例如：
+     * ```json
+     * {
+     *   "ticket": {
+     *     "type": "env",
+     *     "appId": "aaa",
+     *     "secret": "MY_SECRET",
+     *     "token": "MY_TOKEN",
+     *     "plain": true
+     *    }
+     *  }
+     *  ```
+     *
+     * 示例中的 `appId` 并没有找到名为 `aaa` 的 JVM 参数或环境变量，因此它会直接使用 `aaa` 作为 appId。
+     * 而如果 [plain] 为 `false`，则会直接抛出 [IllegalStateException] 异常。
+     *
+     *
+     * 当一个属性以 `PLAIN:` （区分大小写） 为前缀，则会直接使用原始输入值，不会尝试从环境变量中获取。
+     *
+     * 例如：
+     * ```json
+     * {
+     *   "ticket": {
+     *     "type": "env",
+     *     "appId": "PLAIN:aaa",
+     *     "secret": "MY_SECRET",
+     *     "token": "MY_TOKEN",
+     *     "plain": false
+     *   }
+     * }
+     * ```
+     *
+     * 示例中 `appId` 会直接使用 `aaa` 作为 appId，而不会尝试从 JVM 参数或环境变量中获取。
+     *
+     *
+     */
+    @Serializable
+    @SerialName(Simple.TYPE)
+    public data class Env(
+        /**
+         * @see QGBotFileConfiguration.Ticket
+         */
+        val appId: String,
+
+        /**
+         * @see QGBotFileConfiguration.Ticket
+         */
+        val secret: String,
+
+        /**
+         * @see QGBotFileConfiguration.Ticket
+         */
+        val token: String,
+
+        /**
+         * 直接使用
+         */
+        val plain: Boolean = false,
+    ) : TicketConfiguration() {
+
+        /**
+         * @throws IllegalStateException 当 [plain] 为 `false` 时，无法获取到对应的 JVM 参数或环境变量时。
+         */
+        override fun toTicket(): QGBotFileConfiguration.Ticket {
+            val appIdValue = prop("appId", appId)
+            val secretValue = prop("secret", secret)
+            val tokenValue = prop("token", token)
+
+            return QGBotFileConfiguration.Ticket(appIdValue, secretValue, tokenValue)
+        }
+
+        private fun prop(key: String, prop: String): String {
+            if (prop.startsWith(PLAIN_PREFIX)) {
+                return prop.substringAfter(PLAIN_PREFIX, missingDelimiterValue = "")
+            }
+
+            val value = System.getProperty(prop) ?: System.getenv(prop)
+            if (value != null) return value
+
+            if (plain) return prop
+
+            throw IllegalStateException("Cannot find property '$prop' for ticket '$key' in JVM or environment variables.")
+        }
+
+        public companion object {
+            public const val TYPE: String = "env"
+            private const val PLAIN_PREFIX = "PLAIN:"
+        }
+    }
+
 
     public companion object {
         internal val serializersModule = SerializersModule {
