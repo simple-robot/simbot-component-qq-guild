9222d02d960d4e0be68678f70dae7b1d171bacc1
ForteScarlet <ForteScarlet@163.com>
2024-10-17 20:21:57 +0800

    release: v4.1.0

--- simbot-component-qq-guild-stdlib/src/commonTest/kotlin/Ed25519Tests.kt
+++ simbot-component-qq-guild-stdlib/src/commonTest/kotlin/Ed25519Tests.kt
@@ -58,7 +58,7 @@ class Ed25519TestsCommon {
     fun ed25519VerifyTest() {
         // val appId = "11111111"
         val secret = "DG5g3B4j9X2KOErG"
-        val seed   = secret.paddingEd25519Seed()
+        val seed = secret.paddingEd25519Seed()
         val plainToken = "Arq0D5A61EgUu4OxUvOp"
         val ts = "1725442341"
         // val body = """{"d":{"plain_token":"Arq0D5A61EgUu4OxUvOp","event_ts":"1725442341"},"op":13}"""
@@ -85,9 +85,17 @@ class Ed25519TestsCommon {
         )
 
         assertIs<EmitResult.Verified>(result)
-        assertEquals("87befc99c42c651b3aac0278e71ada338433ae26fcb24307bdc5ad38c1adc2d01bcfc" +
-                "adc0842edac85e85205028a1132afe09280305f13aa6909ffc2d652c706", result.verified.signature)
-        assertEquals("Arq0D5A61EgUu4OxUvOp", result.verified.plainToken)
+        assertEquals(
+            "87befc99c42c651b3aac0278e71ada338433ae26fcb24307bdc5ad38c1adc2d01bcfc" +
+                    "adc0842edac85e85205028a1132afe09280305f13aa6909ffc2d652c706",
+            result.verified.signature,
+            "Verified signature not equal"
+            )
+        assertEquals(
+            "Arq0D5A61EgUu4OxUvOp",
+            result.verified.plainToken,
+            "Verified plainToken not equal"
+        )
     }
 
     companion object {
