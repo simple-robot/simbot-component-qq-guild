7464169d8e97889024c3b6bbe1ace05f5fb00886
ForteScarlet <ForteScarlet@163.com>
2024-10-17 19:56:42 +0800

    more tests

--- simbot-component-qq-guild-stdlib/src/commonTest/kotlin/Ed25519Tests.kt
+++ simbot-component-qq-guild-stdlib/src/commonTest/kotlin/Ed25519Tests.kt
@@ -1,10 +1,13 @@
 import io.github.andreypfau.curve25519.ed25519.Ed25519
 import io.ktor.utils.io.core.*
+import kotlinx.coroutines.test.runTest
+import love.forte.simbot.qguild.stdlib.BotFactory
+import love.forte.simbot.qguild.stdlib.EmitResult
 import love.forte.simbot.qguild.stdlib.internal.paddingEd25519Seed
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import kotlin.test.assertIs
 
 /**
  *
@@ -50,31 +53,6 @@ class Ed25519TestsCommon {
         )
     }
 
-    // is OK
-    @OptIn(ExperimentalStdlibApi::class)
-    private fun a() {
-        val secret = "DG5g3B4j9X2KOErG"
-        val seed   = secret.paddingEd25519Seed()
-        val body = """{"d":{"plain_token":"Arq0D5A61EgUu4OxUvOp","event_ts":"1725442341"},"op":13}"""
-        val timestamp = "1725442341"
-        val sig =
-            "865ad13a61752ca65e26bde6676459cd36cf1be609375b37bd62af366e1dc25a8dc789ba7f14e017ada3d554c671a911bfdf075ba54835b23391d509579ed002"
-
-
-        val sigBytes = sig.hexToByteArray()
-
-        assertEquals(Ed25519.SIGNATURE_SIZE_BYTES, sigBytes.size)
-        assertEquals(0u, sigBytes[63].toUByte().and(224u))
-
-        val pk = Ed25519.keyFromSeed(seed.toByteArray())
-        val msg = "$timestamp$body"
-
-        println(pk.sign(msg.toByteArray()).toHexString())
-        println(pk.sign(msg.toByteArray()).toHexString())
-
-        assertTrue { pk.publicKey().verify(msg.toByteArray(), sigBytes) }
-    }
-
     @OptIn(ExperimentalStdlibApi::class)
     @Test
     fun ed25519VerifyTest() {
@@ -96,10 +74,27 @@ class Ed25519TestsCommon {
         )
     }
 
+    @Test
+    fun opcode13ProcessTest() = runTest {
+        val bot = BotFactory.create("11111111", "DG5g3B4j9X2KOErG", "") {
+            disableWs = true
+        }
+
+        val result = bot.emitEvent(
+            """{"d":{"plain_token":"Arq0D5A61EgUu4OxUvOp","event_ts":"1725442341"},"op":13}""",
+        )
+
+        assertIs<EmitResult.Verified>(result)
+        assertEquals("87befc99c42c651b3aac0278e71ada338433ae26fcb24307bdc5ad38c1adc2d01bcfc" +
+                "adc0842edac85e85205028a1132afe09280305f13aa6909ffc2d652c706", result.verified.signature)
+        assertEquals("Arq0D5A61EgUu4OxUvOp", result.verified.plainToken)
+    }
+
     companion object {
         private const val ASSERT_PUBLIC_HEX = "d7c362fe78aef81ff23287b493628b5db02a3c4fe30b215e4d19609b5d76673a"
         private const val ASSERT_PRIVATE_HEX =
-            "6e614f43306f635145337368574c41666666564c42317268595047376e614f43d7c362fe78aef81ff23287b493628b5db02a3c4fe30b215e4d19609b5d76673a"
+            "6e614f43306f635145337368574c41666666564c42317268595047376e614f43" +
+                    "d7c362fe78aef81ff23287b493628b5db02a3c4fe30b215e4d19609b5d76673a"
     }
 }
 
