e0b25bf956d9d177f0dff111dbf6204f7faa41c0
ForteScarlet <ForteScarlet@163.com>
2024-10-17 19:57:44 +0800

    release: v4.1.0

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -57,7 +57,7 @@ object P {
 
 
         const val VERSION = "4.1.0"
-        const val NEXT_VERSION = "4.1.0"
+        const val NEXT_VERSION = "4.1.1"
 
         override val snapshotVersion = "$NEXT_VERSION-SNAPSHOT"
         override val version = if (isSnapshot()) snapshotVersion else VERSION
